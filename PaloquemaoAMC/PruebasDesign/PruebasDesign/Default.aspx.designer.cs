ae65d8f194060219a0fb754d1a7a2136d7d7d63f
Jefferson <jefferson.c@misena.edu.co>
2014-06-02 21:18:43 -0500

    Modificaion de los estilos
    Se modifica los estilos del formulario de ingreso de productos, se
    agrega el modulo de inicio de sesión

--- PaloquemaoAMC/PruebasDesign/PruebasDesign/Default.aspx.designer.cs
+++ PaloquemaoAMC/PruebasDesign/PruebasDesign/Default.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace PaloquemaoACM {
         protected global::System.Web.UI.WebControls.TextBox txbBuscar;
         
         /// <summary>
+        /// Control btnBuscar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnBuscar;
+        
+        /// <summary>
         /// Control tableNav.
         /// </summary>
         /// <remarks>
