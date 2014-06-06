5152fe15a1cb378ea32367afe3c437141ebb26da
chaquen <edgar.guzman21@gmail.com>
2014-06-05 22:02:59 -0500

    Se agrega un loginView

--- PaloquemaoAMC/PruebasDesign/PruebasDesign/MasterPage.Master.cs
+++ PaloquemaoAMC/PruebasDesign/PruebasDesign/MasterPage.Master.cs
@@ -4,14 +4,50 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
+using System.Security.Principal;
 
 namespace PaloquemaoACM
 {
     public partial class MasterPage : System.Web.UI.MasterPage
     {
+        string struser;
+        string strpass;
+        public IPrincipal User { get; set; }       
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void logUser_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+
+            TextBox txbUser = (TextBox)LoginView.FindControl("logUser").FindControl("UserName");
+            TextBox txbPass = (TextBox)LoginView.FindControl("logUser").FindControl("password");
+            struser = txbUser.Text;
+            strpass = txbPass.Text;
+            if (Membership.ValidateUser(struser, strpass))
+            {
+                e.Authenticated = true;
+            }
+            else 
+            {
+                e.Authenticated = false;
+            }
+        }
+
+        protected void logUser_LoggedIn(object sender, EventArgs e)
+        {
+            MembershipUser objMemeberShip = Membership.GetUser(struser);
+
+            if (Roles.IsUserInRole(objMemeberShip.UserName, "admin"))
+            {
+                Response.Redirect("/Default.aspx");
+            }
+            else 
+            {
+                Response.Redirect("/Default.aspx#image-1");
+            }
         }
     }
 }
--- PaloquemaoAMC/PruebasDesign/PruebasDesign/MasterPage.Master.designer.cs
+++ PaloquemaoAMC/PruebasDesign/PruebasDesign/MasterPage.Master.designer.cs
@@ -22,13 +22,22 @@ namespace PaloquemaoACM {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// Control logUser.
+        /// Control LoginView1.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Login logUser;
+        protected global::System.Web.UI.WebControls.LoginView LoginView1;
+        
+        /// <summary>
+        /// Control LoginView.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LoginView LoginView;
         
         /// <summary>
         /// Control Default.
